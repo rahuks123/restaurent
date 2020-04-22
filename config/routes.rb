aa0024660ec5174dacff9eff9a61b6ecb72adb0e
rahul <rahuks123@gmail.com>
2020-04-22 19:49:55 +0530

    included signin and signout page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items
   resources :users
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
   get "/" => "home#index"
 end
